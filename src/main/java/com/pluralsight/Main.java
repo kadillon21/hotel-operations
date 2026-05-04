3d6e8fc9a861c37cbff968105c7fc6ce4bb031e9
kadillon21 <kadillon21@gmail.com>
2026-05-04 14:31:41 -0500

    Constructor and getters created for Room.java

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,9 @@
 package com.pluralsight;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
 }
--- src/main/java/com/pluralsight/Room.java
+++ src/main/java/com/pluralsight/Room.java
@@ -1,4 +1,38 @@
 package com.pluralsight;
 
 public class Room {
+
+    private int numberOfBeds;
+    private int price;
+    private boolean occupied;
+    private boolean dirty;
+    private boolean available;
+
+    public Room(int numberOfBeds, int price, boolean occupied, boolean dirty, boolean available) {
+        this.numberOfBeds = numberOfBeds;
+        this.price = price;
+        this.occupied = occupied;
+        this.dirty = dirty;
+        this.available = available;
+    }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public boolean isOccupied() {
+        return occupied;
+    }
+
+    public boolean isDirty() {
+        return dirty;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
 }
