620e46c65f5e0095d127554b696ecc6e4c9b8bcc
kadillon21 <kadillon21@gmail.com>
2026-05-04 15:06:26 -0500

    Constructor and getters created for Employee.java Added classes to model package

---
+++ src/main/java/com/pluralsight/models/Employee.java
@@ -0,0 +1,56 @@
+package com.pluralsight.models;
+
+public class Employee {
+
+    private int employeeID;
+    private String name;
+    private String department;
+    private double payRate;
+    private double hoursWorked;
+
+    public Employee(int employeeID, String name, String department, double payRate, double hoursWorked) {
+        this.employeeID = employeeID;
+        this.name = name;
+        this.department = department;
+        this.payRate = payRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public double getTotalPay(){
+        return (getOvertimeHours() * (payRate * 1.5)) + (getRegularHours() * payRate);
+    }
+
+    public double getRegularHours() {
+        if (this.hoursWorked > 40){
+            return 40;
+        }
+        return this.hoursWorked;
+    }
+
+    public double getOvertimeHours(){
+        if (this.hoursWorked > 40){
+            return this.hoursWorked - 40;
+        }
+        return 0;
+    }
+}
